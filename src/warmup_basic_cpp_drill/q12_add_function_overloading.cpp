d8310d0c19195b009bf7693f7552ce38a3edbb69
brucekimrokcmu <kwangkyk@alumni.cmu.edu>
2026-07-09 21:30:02 -0700

    solve until q14

--- src/warmup_basic_cpp_drill/q12_add_function_overloading.cpp
+++ src/warmup_basic_cpp_drill/q12_add_function_overloading.cpp
@@ -1,1 +1,9 @@
 // Write two overloaded Add functions: one for int, one for double.
+
+int Add(int a, int b) {
+    return a + b;
+}
+
+double Add(double a, double b) {
+    return a + b;
+}
--- tests/warmup_basic_cpp_drill/q14_test.sh
+++ tests/warmup_basic_cpp_drill/q14_test.sh
@@ -6,6 +6,6 @@ source_path="$1"
 output_path="$2"
 cxx="$3"
 
-require_pattern "${source_path}" '\\bfriend\\b' 'friend function usage'
-require_pattern "${source_path}" '\\bprivate\\b' 'a private field'
+require_pattern "${source_path}" '\bfriend\b' 'friend function usage'
+require_pattern "${source_path}" '\bprivate\b' 'a private field'
 compile_source "${source_path}" "${output_path}" "${cxx}"
